087360d6092c9adde4d4bee99c5901737053c828
Chan Nguyen <atbl1511@gmail.com>
2014-04-23 22:08:51 -0700

    Initial Commit

---
+++ OpenGLESExample/OGEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OGEAppDelegate.h
+//  OpenGLESExample
+//
+//  Created by chan on 4/23/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OGEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenGLESExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenGLESExample
+//
+//  Created by chan on 4/23/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OGEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OGEAppDelegate class]));
+    }
+}
